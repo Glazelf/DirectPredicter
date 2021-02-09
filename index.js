ca9389e98ec4e62264328836b270d55133a4085d
Glazelf <gideon.steenbergen@gmail.com>
2021-02-09 18:06:49 +0100

    remove readline-sync

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const Twitter = require('twitter-lite');
 const config = require('./config.json');
-const readline = require('readline-sync');
 
 const client = new Twitter(config);
+
+let tweetText = "There will be a Nintendo Direct tomorrow"
+if (tweetText.length > 280) {
+    return console.log("Tweet body can only be 280 characters long.");
+} else if (tweetText.length < 1) {
+    return console.log("Tweet body has to be at least 1 character long.");
+};
+
 
 // Adaptive tweet text and checks
 
 // Set body to an object
 let postBody = {
-    'status': "There will be a Nintendo Direct tomorrow."
+    'status': tweetText
 };
 
 // Tweet
